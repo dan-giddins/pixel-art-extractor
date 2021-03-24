81cb5a84f27c725a57bd9691c4d5a41025b9e8a9
Dan Giddins <daniel.giddins112@gmail.com>
2021-03-24 15:06:12 +0000

    kind of working

--- PixelArtExtractor.py
+++ PixelArtExtractor.py
@@ -9,8 +9,22 @@ def printImg(img):
 img = cv2.imread('rotated_cat.png')
 img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
 
-edges = cv2.Canny(img,20,50,L2gradient = True)
+red = img[:,:,0] 
+green = img[:,:,1]
+blue = img[:,:,2]
 
-corners = cv2.cornerHarris(edges, 3, 3, 0.01)
+block_size = 2
+k_size = 3
+k = 0
 
-printImg(corners)
+red_corners = cv2.cornerHarris(red,block_size,k_size,k)
+green_corners = cv2.cornerHarris(green,block_size,k_size,k)
+blue_corners = cv2.cornerHarris(blue,block_size,k_size,k)
+
+corners = numpy.add(numpy.add(red_corners, green_corners), blue_corners)
+
+threshold = numpy.where(corners > 0.00001, 1, 0)
+
+printImg(threshold)
+
+
