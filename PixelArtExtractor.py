0ed930b0b265808829f16dff198ad79577cc17fc
Dan Giddins <daniel.giddins112@gmail.com>
2021-03-24 19:36:02 +0000

    all looking good

--- PixelArtExtractor.py
+++ PixelArtExtractor.py
@@ -1,6 +1,8 @@
-import cv2 as cv
+import cv2
 import numpy as np
 from matplotlib import pyplot as plot
+import math
+import scipy.cluster.hierarchy as hcluster
 
 def printImg(img):
     plot.imshow(img)
@@ -8,7 +10,7 @@ def printImg(img):
 
 def showFeatures(img, features):
     for feature in features:
-        cv.circle(img, (int(feature[0,0]), int(feature[0,1])), 8, (0,0,255))
+        cv2.circle(img, (int(feature[0,0]), int(feature[0,1])), 8, (0,0,255))
 
 def showPoints(img, points):
     array = []
@@ -23,8 +25,37 @@ def showPoints(img, points):
 def convertToSet(features):
     return set(map(tuple, features.reshape(-1,2)))
 
+def convertToArray(s):
+    array = []
+    for i in s:
+        array.append([i[0], i[1]])
+    return array
+
+def groupPoints(corners):
+    theshold_distance = 4
+    grouped_corners = set()
+    for c1 in corners:
+        local_corners = set()
+        for c2 in corners:
+            if (math.hypot(c2[0] - c1[0], c2[1] - c1[1]) < theshold_distance):
+                local_corners.add(c2)
+        x = 0
+        y = 0
+        for c in local_corners:
+            x += c[0]
+            y += c[1]
+        x /= len(local_corners)
+        y /= len(local_corners)
+        grouped_corners.add((x, y))
+    return grouped_corners
+
+def showPointsOnImg(img, points):
+    for point in points:
+        img[int(point[1]), int(point[0])] = [0, 0, 0]
+    printImg(img)
+
 # read source img
-img = cv.imread('rotated_cat.png')
+img = cv2.imread('rotated_cat.png')
 
 # split into channels
 red = img[:,:,2] 
@@ -33,21 +64,48 @@ blue = img[:,:,0]
 
 # goodFeaturesToTrack parms
 max_corners = 0
-qualityLevel = 0.01
-minDistance = 0
+quality_level = 0.01
+min_distance = 0
+block_size = 3
 
 # get features
-red_corners = convertToSet(cv.goodFeaturesToTrack(red,max_corners,qualityLevel,minDistance))
-green_corners = convertToSet(cv.goodFeaturesToTrack(green,max_corners,qualityLevel,minDistance))
-blue_corners = convertToSet(cv.goodFeaturesToTrack(blue,max_corners,qualityLevel,minDistance))
+red_corners = convertToSet(cv2.goodFeaturesToTrack(red,max_corners,quality_level,min_distance,block_size))
+green_corners = convertToSet(cv2.goodFeaturesToTrack(green,max_corners,quality_level,min_distance,block_size))
+blue_corners = convertToSet(cv2.goodFeaturesToTrack(blue,max_corners,quality_level,min_distance,block_size))
 
 # union channel sets together
 corners = red_corners.union(green_corners).union(blue_corners)
 
-# print corners
-showPoints(img, corners)
+#showPointsOnImg(img, corners)
 
-#threshold = np.where(corners > 0.01*corners.max(), 1, 0)
+# clustering
+corner_array = convertToArray(corners)
+corner_clusters = hcluster.fclusterdata(corner_array, 3, criterion="distance")
 
+corner_groups = [[] for _ in range(corner_clusters.max())]
+for i in range(len(corner_array)):
+    corner_groups[corner_clusters[i]-1].append(corner_array[i])
+
+grouped_corners = set()
+for group in corner_groups:
+    x = 0
+    y = 0
+    for point in group:
+        x += point[0]
+        y += point[1]
+    x /= len(group)
+    y /= len(group)
+    grouped_corners.add((x, y))
+
+#showPointsOnImg(img, grouped_corners)
+
+scale_factor = 6
+scaled_corners = set()
+for c in grouped_corners:
+    scaled_corners.add((c[0]*scale_factor, c[1]*scale_factor))
+h, w, c = img.shape
+scaled_img = cv2.resize(img, (w * scale_factor, h * scale_factor))
+
+#showPoints(scaled_img, scaled_corners)
 
 
